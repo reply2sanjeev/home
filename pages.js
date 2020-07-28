dc71493317e2c2a1ee1df75c7d343e438f3af641
reply2sanjeev <39366579+reply2sanjeev@users.noreply.github.com>
2020-07-28 20:45:24 +0530

    Update pages.js

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/hashirshoaeb/hashirshoaeb.github.io.git";
+const repoURL = "https://github.com/reply2sanjeev/sanjeev.gihub.io.git";
 
 ghpages.publish(
   pathname,
